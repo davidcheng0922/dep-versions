30f180feaef83782841ae9413114dd7c478bc722
David Cheng <davidcheng0922@gmail.com>
2025-12-09 09:41:08 +0800

    feat(spdk): add python3 back for building spdk
    Signed-off-by: David Cheng <davidcheng0922@gmail.com>

--- scripts/build-spdk.sh
+++ scripts/build-spdk.sh
@@ -34,6 +34,10 @@ cd "$SRC_DIR"
 git checkout "$SPDK_COMMIT_ID"
 git submodule update --init
 
+zypper refresh
+sed -i '/python3-pyelftools/d' ./scripts/pkgdep/sles.sh
+sed -i 's/python3-/python311-/g' ./scripts/pkgdep/sles.sh
+
 # Install dependencies
 ./scripts/pkgdep.sh --uring
 pip3 install -r ./scripts/pkgdep/requirements.txt
